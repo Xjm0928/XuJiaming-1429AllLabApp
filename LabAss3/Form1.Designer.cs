5b9494f9b22ca9fde249f9df3d347241bf23c8d6
Xjm0928 <100857486+Xjm0928@users.noreply.github.com>
2022-04-19 10:18:20 +0800

    Add files via upload

---
+++ LabAss3/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace LabAss3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Customer = new System.Windows.Forms.TextBox();
+            this.Country = new System.Windows.Forms.TextBox();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Married = new System.Windows.Forms.RadioButton();
+            this.Unmarried = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Preview = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Country";
+            // 
+            // Customer
+            // 
+            this.Customer.Location = new System.Drawing.Point(234, 29);
+            this.Customer.Name = "Customer";
+            this.Customer.Size = new System.Drawing.Size(121, 25);
+            this.Customer.TabIndex = 2;
+            this.Customer.Text = "Nabil";
+            // 
+            // Country
+            // 
+            this.Country.Location = new System.Drawing.Point(234, 69);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(121, 25);
+            this.Country.TabIndex = 3;
+            this.Country.Text = "Bangladesh";
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Male.Location = new System.Drawing.Point(25, 39);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(60, 19);
+            this.Male.TabIndex = 4;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Location = new System.Drawing.Point(182, 40);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(76, 19);
+            this.Female.TabIndex = 5;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Female);
+            this.groupBox1.Controls.Add(this.Male);
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.groupBox1.Location = new System.Drawing.Point(50, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 78);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(20, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Reading";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(177, 28);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 19);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Painting";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(55, 241);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 68);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hobbies";
+            // 
+            // Married
+            // 
+            this.Married.AutoSize = true;
+            this.Married.Location = new System.Drawing.Point(14, 26);
+            this.Married.Name = "Married";
+            this.Married.Size = new System.Drawing.Size(84, 19);
+            this.Married.TabIndex = 10;
+            this.Married.TabStop = true;
+            this.Married.Text = "Married";
+            this.Married.UseVisualStyleBackColor = true;
+            // 
+            // Unmarried
+            // 
+            this.Unmarried.AutoSize = true;
+            this.Unmarried.Location = new System.Drawing.Point(168, 26);
+            this.Unmarried.Name = "Unmarried";
+            this.Unmarried.Size = new System.Drawing.Size(100, 19);
+            this.Unmarried.TabIndex = 11;
+            this.Unmarried.TabStop = true;
+            this.Unmarried.Text = "Unmarried";
+            this.Unmarried.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Unmarried);
+            this.groupBox3.Controls.Add(this.Married);
+            this.groupBox3.Location = new System.Drawing.Point(61, 354);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(291, 56);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status";
+            // 
+            // Preview
+            // 
+            this.Preview.Location = new System.Drawing.Point(125, 470);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(139, 28);
+            this.Preview.TabIndex = 13;
+            this.Preview.Text = "Preview";
+            this.Preview.UseVisualStyleBackColor = true;
+            this.Preview.Click += new System.EventHandler(this.Preview_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 558);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.Customer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Customer Data Entry Screen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Customer;
+        private System.Windows.Forms.TextBox Country;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton Married;
+        private System.Windows.Forms.RadioButton Unmarried;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Preview;
+    }
+}
+
